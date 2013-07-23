ebef9467d1d12c060e39f1023d36e2a05ecd816e
Vasiliy Litovchenko <muaddeep@gmail.com>
2013-07-23 15:03:39 +0300

    07: references expand, output style fixes

--- model/07/07.pl
+++ model/07/07.pl
@@ -36,7 +36,7 @@ sub dump {
 
 	my $out = "";
 	if($ref eq 'SCALAR'){
-		$out .= $$var . "\n";
+		$out .= "'" .  $$var . "'";
 	}elsif($ref eq 'ARRAY'){
 		$out .= "[" . "\n" 
 		. join(",\n", map("\t" x $indent . &dump($_, $indent + 1), @$var)) 
@@ -46,9 +46,11 @@ sub dump {
 		. join(",\n", map("\t" x $indent. $_ . " => " . &dump($var->{$_}, $indent + 1), sort {uc($a) cmp uc($b)} keys %$var))
 		. "\n". "\t" x ($indent - 1) . "}";
 	}elsif($ref eq 'REF'){
-		$out = "$ref";
+		$out = "REF:" . &dump($$var, $indent);
 	}elsif($ref && UNIVERSAL::can($var, "can") && $ref->can("get_struct")){
 		$out .= $var->get_struct();
+	}elsif($ref){
+		$out .= $ref;
 	}else{
 		$out .= "'$var'";
 	}
