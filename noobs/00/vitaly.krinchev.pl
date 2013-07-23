0c08db120430c32c655242ff927deae9cf2f5e40
cono <q@cono.org.ua>
2013-07-23 21:20:55 +0300

    Fix space issue for vitaly krinchev

--- noobs/00/vitaly.krinchev.pl
+++ noobs/00/vitaly.krinchev.pl
@@ -18,20 +18,20 @@ $_=~s/\n//g;
 			($a,$b,$c)=split(/,/,$_,3);
 			if (($a,$b,$c)!=0){ 
 			$D=$b**2-4*$a*$c;
-			if ($a==0) {$x=($c/$b)*(-1);print STDOUT '1('.$x.')'."\n";}
+			if ($a==0) {$x=($c/$b)*(-1);print STDOUT '1 ('.$x.')'."\n";}
 		else{
 			if ($D>0) 
 				{$x1=(((-1)*$b)-sqrt($D))/(2*$a);
 				 $x2=(((-1)*$b)+sqrt($D))/(2*$a);
-				 print STDOUT '2('.$x1.' '.$x2.')'."\n";}
-			elsif ($D<0) {print STDOUT 'Дискриминант меньше нуля, решением будут являтся комплексные числа'."\n";}
-			else {print STDOUT '1('.(-1)*($b/(2*$a)).')'."\n";}
+				 print STDOUT '2 ('.$x1.' '.$x2.')'."\n";}
+			elsif ($D<0) {print STDERR 'Дискриминант меньше нуля, решением будут являтся комплексные числа'."\n";}
+			else {print STDOUT '1 ('.(-1)*($b/(2*$a)).')'."\n";}
 				
 			}
 	}
-	else {print STDOUT "Не достаточно данных для решения уровнения \n";}
+	else {print STDERR "Не достаточно данных для решения уровнения \n";}
 	}
-	else {print STDOUT "Введены не верные данные \n";}
+	else {print STDERR "Введены не верные данные \n";}
 	print STDERR "...\n" if ( $res eq 'Error' );
 }
 close ( FH );
