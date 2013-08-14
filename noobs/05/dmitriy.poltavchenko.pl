95fc7e0e8045127527953481aa5d295e3d620d7f
ZEN-ROOT <ZEN-ROOT@users.noreply.github.com>
2013-08-14 20:54:37 +0300

    Update dmitriy.poltavchenko.pl
    Replace all code. Before revision is code from 04 script

--- noobs/05/dmitriy.poltavchenko.pl
+++ noobs/05/dmitriy.poltavchenko.pl
@@ -1,98 +1,258 @@
 #!/usr/bin/perl -w
 use strict;
+package MySh;
+sub isReable {
+    if (! -e $_[0]) {
+        return -1;
+    }
+    if (! -r $_[0]) {
+        return -1;
+    }
+    return 0;
+}
+
+sub isWritable {
+    open(my $F, '>', "$_[0]") or return -1;
+    close($F);
+    if (! -w $_[0]) {
+        return -1;
+    }
+    return 0;
+}
+
+sub readAll {
+    my $fileContent;
+    open(my $F, '<', "$_[0]") or die $!;
+    binmode($F);
+    {
+        local $/;
+        $fileContent = <$F>;
+    }
+    close($F);
+    return $fileContent;
+}
+
+sub writeAll {
+    my $fileContent;
+    open(my $F, '>', "$_[0]") or die $!;
+    binmode($F);
+    print $F $_[1];
+    close($F);
+}
+
+sub print {
+    if (scalar(@{$_[0]}) == 1) {
+        return -1;
+    }
+    
+    my $count = 0;
+    foreach my $file(@{$_[0]}) {
+        next if ($count++ == 0);
+        if (($file eq ">") && ($_[0]->[1] ne ">") &&
+            $count == scalar(@{$_[0]})-1) {
+            my $buff = join "", @{$_[1]};
+            if (isWritable($_[0]->[$count]) ne "0") {
+                return -1;
+            }
+            writeAll($_[0]->[$count], $buff);
+            undef @{$_[1]};
+            last;
+        } elsif ($file eq ">") {
+            return -2;
+        }
+        if (isReable($file) ne "0") {
+            return -1;
+        }
+        open (INPUT,"<",$file) or return -1;
+        while (<INPUT>) {
+            push (@{$_[1]}, $_);
+        }
+        close(INPUT);
+    }
+    return 0;
+}
+
+sub delete {
+    my $count = 0;
+    undef @{$_[1]};
+    if (scalar(@{$_[0]}) == 1) {
+        return -2;
+    }
+    
+    foreach my $file(@{$_[0]}) {
+        next if ($count++ == 0);
+        if (isWritable($file) ne "0") {
+            return -1;
+        }
+        unlink $file or return -1;
+    }    
+    return 0;
+}
+
+sub move {
+    undef @{$_[1]};    
+    if (scalar(@{$_[0]}) == 3) {
+        if (! rename ("$_[0]->[1]", "$_[0]->[2]")) {
+            return -1;   
+        }        
+    } else {
+        return -2;
+    }
+    return 0;
+}
+
+sub copy {
+    undef @{$_[1]};
+    if (scalar(@{$_[0]}) == 3) {
+        if (isReable($_[0]->[1]) ne "0") {
+            return -1;
+        }
+        my $tmp = readAll($_[0]->[1]);
+        if (isWritable($_[0]->[2]) ne "0") {
+            return -1;
+        }
+        writeAll($_[0]->[2], $tmp);
+    } else {
+        return -2;
+    }
+    return 0;
+}
+
+sub sort {
+    if (scalar(@{$_[0]}) == 1 && scalar(@{$_[1]}) != 0) {
+        @{$_[1]} = sort { $a cmp $b }@{$_[1]};
+    } elsif (scalar(@{$_[0]}) == 2) {
+        undef @{$_[1]};
+        my $file = $_[0]->[1];
+        if (isReable($file) ne "0") {
+            return -1;
+        }
+        open (INPUT,"<",$file) or return -1;
+        while (<INPUT>) {
+            push (@{$_[1]}, $_);
+        }
+        close(INPUT);
+        @{$_[1]} = sort { $a cmp $b }@{$_[1]};
+    } elsif (scalar(@{$_[0]}) == 4 && ($_[0]->[2] eq ">")) {
+        undef @{$_[1]};
+        my $file = $_[0]->[1];
+        if (isReable($file) ne "0") {
+            return -1;
+        }
+        open (INPUT,"<",$file) or return -1;
+        while (<INPUT>) {
+            push (@{$_[1]}, $_);
+        }
+        close(INPUT);
+        @{$_[1]} = sort { $b cmp $a }@{$_[1]};
+        my $buff = join "", @{$_[1]};
+        if (isWritable($_[0]->[3]) ne "0") {
+            return -1;
+        }
+        writeAll($_[0]->[3], $buff);
+        undef @{$_[1]};
+    } else {
+        return -2;
+    }
+    return 0;
+}
+
+sub search {
+    if (scalar(@{$_[0]}) == 2 && scalar(@{$_[1]}) != 0) {
+        @{$_[1]} = grep(/$_[0]->[1]/, @{$_[1]});
+    } elsif (scalar(@{$_[0]}) == 3) {
+        undef @{$_[1]};
+        my $file = $_[0]->[2];
+        if (isReable($file) ne "0") {
+            return -1;
+        }
+        open (INPUT,"<",$file) or return -1;
+        while (<INPUT>) {
+            push (@{$_[1]}, $_);
+        }
+        close(INPUT);
+        @{$_[1]} = grep(/$_[0]->[1]/, @{$_[1]});
+    } elsif (scalar(@{$_[0]}) == 4 && ($_[0]->[2] eq ">")) {
+        @{$_[1]} = grep(/$_[0]->[1]/, @{$_[1]});
+        my $buff = join "", @{$_[1]};
+        if (isWritable($_[0]->[3]) ne "0") {
+            return -1;
+        }
+        writeAll($_[0]->[3], $buff);
+        undef @{$_[1]};
+    } elsif (scalar(@{$_[0]}) == 5 && ($_[0]->[3] eq ">")) {
+        undef @{$_[1]};
+        my $file = $_[0]->[2];
+        if (isReable($file) ne "0") {
+            return -1;
+        }
+        open (INPUT,"<",$file) or return -1;
+        while (<INPUT>) {
+            push (@{$_[1]}, $_);
+        }
+        close(INPUT);
+        @{$_[1]} = grep(/$_[0]->[1]/, @{$_[1]});
+        my $buff = join "", @{$_[1]};
+        if (isWritable($_[0]->[4]) ne "0") {
+            return -1;
+        }
+        writeAll($_[0]->[4], $buff);     
+        undef @{$_[1]};
+    } else {
+        return -2;
+    }
+    return 0;
+}
+
+package main;
+
+# function list
+my %cmd = (
+    "print" => \&MySh::print,
+    "delete" => \&MySh::delete,
+    "move" => \&MySh::move,
+    "copy" => \&MySh::copy,
+    "sort" => \&MySh::sort,
+    "search" => \&MySh::search,
+    "exit" => sub {exit 0},
+);
 
 # use first argument of script as file name whith data
 my $test_file_path = $ARGV[0];
 # open file and get file handler or die
 open( FH, "<", "$test_file_path") or die "Can not open test file: $!";
-my $first_key = "";
-my $str = "";
-# line counter
-my $count = 0;
-# future hash
-my %values; # original key - value for replace
-my %tmphash; # key [#####] - value for replace
-my $sharp; # [#####] delimetr
+
 while (<FH>) {
-    my $tmp = $_; # remove \s when needet
-    # Remove new line character
-    $tmp =~ s/\n//g;
-
-    if ($count == 0 && !($tmp =~ m/\W/g) && $tmp ne "") { # if first step
-        $first_key = $tmp; # set key
-        $count++;
-        next;
-    } elsif ($count == 0) {
-        print "0\n";
-        warn("Error. No valid data - $tmp\n");
-        exit -1;
-    }
-    
-    $tmp =~ s/^\s+//g;
-    # Remove comments
-    $tmp =~ s/\s*#.*//g;
-    
-    # Skip empty line and comments
-    if ($tmp =~ m/^#/ || $tmp =~ m/^\s*$/) {
-        next;
-    }
-    
-    if ($tmp =~ m/(.+)(=>|=)(.+)/ ) {
-        my ($k, $v) = ($1, $3);
-        $k =~ s/\s+$//;
-        $k =~ s/^\s+//g;
-        $v =~ s/\s+$//;
-        $v =~ s/^\s+//g;
-        # Remove "|' pair for key
-        if ($k =~ m/^'(.+)'$/) {
-            $k =~ s/^'//;
-            $k =~ s/'$//;
-        } elsif ($k =~ m/^"(.+)"$/) {
-            $k =~ s/^"//;
-            $k =~ s/"$//;
-        }
-        # Remove "|' pair for value
-        if ($v =~ m/^'(.+)'$/) {
-            $v =~ s/^'//;
-            $v =~ s/'$//;
-        } elsif ($v =~ m/^"(.+)"$/) {
-            $v =~ s/^"//;
-            $v =~ s/"$//;
-        }
-        # Validate key and value
-        if ($k=~/\W/||$v=~/\W/) {
+    #$_ =~ s/\n//;
+    chomp;
+    print "MySh::>$_"."\n";
+    $_ =~ s/\s+([>]+)/ $1 /;
+    $_ =~ s/(^\s+)|(\s+\$)|([\s]{2,})/ /g;    
+    my @array = split(/\s+\|\s+/, $_);
+    my $counter = 0;
+    my @tmp;
+    foreach my $item(@array) {
+        my @buff = split(/ /, $item);
+        
+        $_ = $item;
+        
+        if ($cmd{$buff[0]}) {
+            my $status = $cmd{$buff[0]}->(\@buff, \@tmp);
+            if ($status ne "0") {
+                print "0\n";
+                warn("Error code: $status in command: \'$item\'");
+                last;
+            } 
+        } else {
             print "0\n";
-            warn("Error. Key or value missmath - $k=>$v\n");
-            exit -2;
+            warn("Command not found");            
         }
-        # fill hashes
-        $values{$k} = $v;
-        $sharp .= "#";
-        $tmphash{$v} = $sharp;
-        #print "$k\t=>\t$v\n";
-    } elsif (!($tmp =~ m/^(\s*?)$/ || $tmp =~ m/^\s*\#.*+/)) {
-        print "0\n";
-        warn("Error. Data missmath - $tmp\n");
-        exit -3;
-    } # else comment or empty line
-    $count++;
-}
 
-#if (scalar(keys %tmphash) < 1) {
-#    print "0\n";
-#    warn("Error. No hash found\n");
-#    exit -4;
-#}
-
-# Replace keys as [#]..[##]
-for my $key (sort {$b cmp $a} keys %values) {
-    $first_key =~ s/$key/\[$tmphash{$values{$key}}\]/g;
+        if (++$counter == scalar(@array)) {
+            print @tmp;
+        }
+    }
 }
-# Replace [#]..[##] as values
-for my $key (keys %values) {
-    $first_key =~ s/\[$tmphash{$values{$key}}\]/$values{$key}/g;
-}
-
-print $first_key."\n";
+print "MySh::>"."\n";
 
 exit 0;
