41230250875a4776f116645eb11bed0e5afa6d99
MrThieff <net-thief@ya.ru>
2013-08-16 04:00:07 +0300

    Update dima.mukaeliants.pl
    Please accept my changes. Dmitriy Mukaeliants.

--- noobs/03/dima.mukaeliants.pl
+++ noobs/03/dima.mukaeliants.pl
@@ -8,8 +8,8 @@ my $dataString = '';
 my @arr = ();
 my @resArr = ();
 my %hash = ();
-#перед созданием хэша нужно проверить на наличие одинаковых ключей
-#if(){print "!!!	Too much Entry point."}		СКАЗАЛИ ЧТО ЛИШНЯЯ ПРОВЕРКА
+#РїРµСЂРµРґ СЃРѕР·РґР°РЅРёРµРј С…СЌС€Р° РЅСѓР¶РЅРѕ РїСЂРѕРІРµСЂРёС‚СЊ РЅР° РЅР°Р»РёС‡РёРµ РѕРґРёРЅР°РєРѕРІС‹С… РєР»СЋС‡РµР№
+#if(){print "!!!	Too much Entry point."}		РЎРљРђР—РђР›Р Р§РўРћ Р›РРЁРќРЇРЇ РџР РћР’Р•Р РљРђ
 my $test_file_path = $ARGV[0];
 open( FH, "<", "$test_file_path") or die "Can not open test file: $!";
 while ( defined(my $file_string = <FH>)) {
@@ -30,23 +30,23 @@ while ( defined(my $file_string = <FH>)) {
 close ( FH );
 
 if(!$entry || !$dataString){
-	$res = "error";
+	$res = "error\n";
 	$errType = "Incorrect Data in this file!";
 }
 elsif($entry !~ m/^\s*?\w+?\s*?$/){
-	$res = "error";
+	$res = "error\n";
 	$errType = "Incorrect Entry point.";
 }
 else{
 	@arr = split(',',$dataString);
 	for(my $i=0;$i < scalar(@arr);$i++){
 		if($arr[$i]!~/\s*?\w+?\s*?=>\s*?\w+?\s*?/){
-			$res = "error";
+			$res = "error\n";
 			$errType = "Incorrect Hash. [wrong way data: '$arr[$i]']";
 			last;
 		}
 		elsif($arr[$i]=~/.+?=.+?=.+?/){
-			$res = "error";
+			$res = "error\n";
 			$errType = "Incorrect Hash. [two or more '=>' at one hash waypoint: '$arr[$i]']";
 			last;
 		}
@@ -78,7 +78,7 @@ if(!$res){
 			push (@resArr, $hash{$tmpKey});
 		}
 		else{
-			$res = 'error';
+			$res = "error\n";
 			$errType = "Couldn't find next waypoint in the Hash. Way is interrupted: ".join('-',@resArr)."-!!!";
 			last;
 		}
@@ -86,7 +86,7 @@ if(!$res){
 }		
 if(!$res || $res=~/^looped/){$res = join('-',@resArr)."\n".$res."\n"};
 
-print STDOUT "$res\n";
+print STDOUT "$res";
 print STDERR "ERROR: $test_file_path : $errType \n\n" if ($res=~/(error|looped)/);	
 
 
